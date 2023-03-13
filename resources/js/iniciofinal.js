9de3fb0681c51895c8e08993288ddab8f368fbd7
Rober008 <20210722@ricaldone.edu.sv>
2023-03-12 18:47:52 -0600

    Login y register terminado y comentado

--- resources/js/iniciofinal.js
+++ resources/js/iniciofinal.js
@@ -3,6 +3,7 @@ const toggleBtn = document.querySelector('.toggle_btn')
 const toggleBtnIcon = document.querySelector('.toggle_btn i')
 const dropDownMenu = document.querySelector('.dropdown_menu')
 
+/* se le asigna que al momento de dar click, el menu burger se despliegue */
 toggleBtn.onclick = function() {
     dropDownMenu.classList.toggle('open')
 
--- resources/js/public/loginregister.js
+++ resources/js/public/loginregister.js
@@ -1,7 +1,8 @@
+/*Funcionalidad delos botones "registrarse" e "Iniciar sesion"*/
 const  wrapper = document.querySelector('.wrapper');
 const  loginLink = document.querySelector('.login-link');
 const  registerLink = document.querySelector('.register-link');
-
+/*Al momento de dar clic en los botones, remueve los elementos de uno para que el otro tome su  lugar */
 registerLink.addEventListener('click', ()=>{
     wrapper.classList.add('active');
 });
