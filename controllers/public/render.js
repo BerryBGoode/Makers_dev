43eaba4a5364eb30b2b0affb947cf82396ce41f2
BerryBGoode <fernandomena3131@gmail.com>
2023-03-18 10:45:29 -0600

    nuevo nav renderizado, footer en render.js

--- controllers/public/render.js
+++ controllers/public/render.js
@@ -1,47 +1,38 @@
-const header = document.getElementById('render-header');
-header.innerHTML = `<div class="row">
-    <div class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <div class="navbar">
-            <!--Logo y links que redireccionan a las otras páginas web-->
-            <div class="logo">
-                <a href="#"><img src="../../resources/img/logo_blanco.png" alt="" /></a>
-            </div>
-            <ul class="links">
-                <li><a href="index.html">INICIO</a></li>
-                <li><a href="reservacion.html">RERSERVAS</a></li>
-                <li><a href="productos.html">PRODUCTOS</a></li>
-                <li><a href="servicios.html">SERVICIOS</a></li>
-            </ul>
-            <a href="login.html" class="action_btn"> INICIAR SESION </a>
-            <div class="toggle_btn">
-                <svg class="ham hamRotate180 ham5" id="burger" viewBox="0 0 100 100" width="80" onclick="toogle();">
-                    <path class="line top"/>
-                    <path class="line middle" d="m 70,50  h -40" />
-                    <path class="line bottom" d="m 30,67 h 40 c  
-                        -6.122707,-9.002293 -8.5,-6.625 l -11.071429" />
-                </svg>
-            </div>
-        </div>
-        <!--Menú burger para dispositivos pequeños-->
-        <div class="dropdown_menu">
-            <li><a href="index.html">INICIO</a></li>
-            <li><a href="reservacion.html">RERSERVAS</a></li>
-            <li><a href="productos.html">PRODUCTOS</a></li>
-            <li><a href="servicios.html">SERVICIOS</a></li>
-            <li>
-                <a href="login.html" class="action_btn"> INICIAR SESION </a>
-            </li>
-        </div>
-        <div class="pato"></div>
+const nav = document.getElementById('render-nav');
+
+nav.innerHTML = `<nav class="nav">
+    <div class="logo">
+        <img src="../../resources/img/logo_blanco.png" alt="logo">
     </div>
-</div>`;
+    <div class="opciones">
+        <ol>
+            <li><a href="../../views/public/index.html">INICIO</a></li>
+            <li><a href="../../views/public/productos.html">PRODUCTOS</a></li>
+            <li><a href="../../views/public/servicios.html">SERVICIOS</a></li>
+            <li><a href="../../views/public/reservacion.html">RESERVAR</a></li>
+        </ol> 
+        <div class="iniciar-sesion">
+            <a href="">INICIAR SESION</a>
+        </div>           
+    </div>
+    <div class="menu-hamgurguesa">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>    
+    </nav>
+    <div class="curva"></div>`;
 
 
-const footer = document.querySelector('footer');
-if (footer !== null) {
-    footer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+const footer_sc = document.querySelector('footer');
+if (footer_sc !== null) {
+    footer_sc.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
     <path fill="#393534" fill-opacity="2"
         d="M0,192L120,176C240,160,480,128,720,106.7C960,85,1200,75,1320,69.3L1440,64L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z">
     </path>
     </svg>`;
 }
+const footer = document.getElementById('render-footer');
+footer.innerHTML = `<footer class="footer">
+    <span>2023 Makers @copyright</span>
+</footer>`;
