807e3d830132c0901a987fc816ed6faf3c2a9d18
BerryBGoode <fernandomena3131@gmail.com>
2023-05-03 18:24:22 -0600

    cambio de estructura de recursos/imagenes (5/3/2023)

--- controllers/public/productos.js
+++ controllers/public/productos.js
@@ -1,31 +1,31 @@
 var multipleCardCarousel = document.querySelector(
     "#carouselproductos"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
+        interval: false,
     });
     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
     var cardWidth = $(".carousel-item").width();
     var scrollPosition = 0;
     $("#carouselproductos .carousel-control-next").on("click", function () {
-      if (scrollPosition < carouselWidth - cardWidth * 4) {
-        scrollPosition += cardWidth;
-        $("#carouselproductos .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouselproductos .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
     });
     $("#carouselproductos .carousel-control-prev").on("click", function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $("#carouselproductos .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            $("#carouselproductos .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
     });
-  } else {
+} else {
     $(multipleCardCarousel).addClass("slide");
-  }
+}
--- controllers/public/servicios.js
+++ controllers/public/servicios.js
@@ -1,63 +1,63 @@
 var multipleCardCarousel = document.querySelector(
     "#carouseldestacados"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
+        interval: false,
     });
     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
     var cardWidth = $(".carousel-item").width();
     var scrollPosition = 0;
     $("#carouseldestacados .carousel-control-next").on("click", function () {
-      if (scrollPosition < carouselWidth - cardWidth * 4) {
-        scrollPosition += cardWidth;
-        $("#carouseldestacados .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouseldestacados .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
     });
     $("#carouseldestacados .carousel-control-prev").on("click", function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $("#carouseldestacados .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            $("#carouseldestacados .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
     });
-  } else {
+} else {
     $(multipleCardCarousel).addClass("slide");
-  }
+}
 
-  var multipleCardCarousel = document.querySelector(
+var multipleCardCarousel = document.querySelector(
     "#carouselcorte"
-  );
-  if (window.matchMedia("(min-width: 768px)").matches) {
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
-      interval: false,
+        interval: false,
     });
     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
     var cardWidth = $(".carousel-item").width();
     var scrollPosition = 0;
     $("#carouselcorte .carousel-control-next").on("click", function () {
-      if (scrollPosition < carouselWidth - cardWidth * 4) {
-        scrollPosition += cardWidth;
-        $("#carouselcorte .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
+        if (scrollPosition < carouselWidth - cardWidth * 4) {
+            scrollPosition += cardWidth;
+            $("#carouselcorte .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
     });
     $("#carouselcorte .carousel-control-prev").on("click", function () {
-      if (scrollPosition > 0) {
-        scrollPosition -= cardWidth;
-        $("#carouselcorte .carousel-inner").animate(
-          { scrollLeft: scrollPosition },
-          600
-        );
-      }
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            $("#carouselcorte .carousel-inner").animate(
+                { scrollLeft: scrollPosition },
+                600
+            );
+        }
     });
-  } else {
+} else {
     $(multipleCardCarousel).addClass("slide");
-  }
+}
